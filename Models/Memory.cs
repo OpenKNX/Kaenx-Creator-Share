6408f5ea12f0081a65b243d4d548121b27f29e54
olterion <o.stefan252@googlemail.com>
2024-09-19 08:20:05 +0200

    throw new exception to catch in GUI
    To show a GUI message for memory overflow in calculationg the heatmap, now there is thown a new exception.

--- Models/Memory.cs
+++ Models/Memory.cs
@@ -143,9 +143,9 @@ namespace Kaenx.Creator.Models
                     MemorySection sec = Sections.Single(s => s.Address == secAddr);
                     int byteIndex = paraAddr - secAddr;
                     sec.Bytes[byteIndex].SetByteUsed(usage, usedBy);
-                }catch(Exception e){
-                    if (e is InvalidOperationException){
-                        return;
+                }catch(Exception ex){
+                    if (ex is InvalidOperationException){
+                        throw new Exception("out_of_memory");
                     }
                 }
             }
